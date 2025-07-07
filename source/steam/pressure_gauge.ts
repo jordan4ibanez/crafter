7b8df7cbc5113ac71933a71c9b98a7f41a6d8468
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-07 00:18:48 -0400

    Fuck not having entity uuids indeed

--- source/steam/pressure_gauge.ts
+++ source/steam/pressure_gauge.ts
@@ -1,14 +1,49 @@
 namespace steam {
+	const pressureGaugeEntities = new Map<number, ObjectRef>();
+
+	class PressureGaugeEntity extends types.Entity {
+		name: string = "crafter_steam:firebox_fire_entity";
+		initial_properties: ObjectProperties = {
+			pointable: false,
+			visual: EntityVisual.mesh,
+			mesh: "steam_pressure_gauge.gltf",
+			textures: [
+				"steam_pressure_gauge_dial.png",
+				"steam_pressure_gauge_bracket.png",
+				"steam_pressure_gauge_inlet.png",
+				"steam_pressure_gauge_needle.png",
+				"stone.png",
+			],
+			visual_size: vector.create3d(1, 1, 1),
+			static_save: false,
+		};
+	}
+	utility.registerTSEntity(PressureGaugeEntity);
+
+	/**
+	 * At the time of writing this, entities have no UUID.
+	 * This fucking hackjob is getting around this issue.
+	 * FUCK not having UUIDs.
+	 */
+	function getOrCreateEntity(pos: Vec3): ObjectRef | null {
+		const hash = core.hash_node_position(pos);
+		let entity = pressureGaugeEntities.get(hash) || null;
+		if (entity == null || !entity.is_valid()) {
+			entity = core.add_entity(pos, "crafter_steam:firebox_fire_entity");
+			if (entity == null || !entity.is_valid()) {
+				core.log(
+					LogLevel.error,
+					`Failed to add firebox entity at ${pos}`
+				);
+				return null;
+			}
+		}
+		pressureGaugeEntities.set(hash, entity);
+		return entity;
+	}
+
 	core.register_node("crafter_steam:pressure_gauge", {
-		drawtype: Drawtype.mesh,
-		tiles: [
-			"steam_pressure_gauge_dial.png",
-			"steam_pressure_gauge_bracket.png",
-			"steam_pressure_gauge_inlet.png",
-			"steam_pressure_gauge_needle.png",
-			"stone.png",
-		],
-		mesh: "steam_pressure_gauge.gltf",
+		drawtype: Drawtype.airlike,
 		sounds: crafter.stoneSound(),
 		groups: { stone: 2 },
 		paramtype: ParamType1.light,
