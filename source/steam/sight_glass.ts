c8061ce3267e3961db6e4515a9a80262a3cacd90
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-07 19:02:10 -0400

    Update sight_glass.ts

--- source/steam/sight_glass.ts
+++ source/steam/sight_glass.ts
@@ -1,12 +1,86 @@
 namespace steam {
+	const timerStart = kickOnSteamNodeTimer;
+
+	const sightGlassEntities = new Map<number, ObjectRef>();
+
+	class SightGlassEntity extends types.Entity {
+		name: string = "crafter_steam:sight_glass_entity";
+		initial_properties: ObjectProperties = {
+			pointable: false,
+			visual: EntityVisual.mesh,
+			mesh: "steam_sight_glass.gltf",
+			textures: [
+				"steam_sight_glass.png",
+				"steam_sight_glass_water_texture.png",
+			],
+			visual_size: vector.create3d(1, 1, 1),
+			static_save: false,
+		};
+	}
+	utility.registerTSEntity(SightGlassEntity);
+
+	/**
+	 * At the time of writing this, entities have no UUID.
+	 * This fucking hackjob is getting around this issue.
+	 * FUCK not having UUIDs.
+	 */
+	function getOrCreateEntity(pos: Vec3): ObjectRef | null {
+		const hash = core.hash_node_position(pos);
+		let entity = sightGlassEntities.get(hash) || null;
+		if (entity == null || !entity.is_valid()) {
+			entity = core.add_entity(pos, "crafter_steam:sight_glass_entity");
+			if (entity == null || !entity.is_valid()) {
+				core.log(
+					LogLevel.error,
+					`Failed to add sight glass entity at ${pos}`
+				);
+				return null;
+			}
+		}
+		const param2 = core.get_node(pos).param2;
+		if (param2 != null) {
+			entity.set_yaw(core.dir_to_yaw(core.fourdir_to_dir(param2)));
+		} else {
+			core.log(LogLevel.error, `Param2 at ${pos} doesn't exist.`);
+		}
+		sightGlassEntities.set(hash, entity);
+		return entity;
+	}
+
+	function manipulateSightGlassEntity(
+		pos: Vec3,
+		entity: ObjectRef | null
+	): void {
+		if (entity == null) {
+			return;
+		}
+	}
+
 	core.register_node("crafter_steam:sight_glass", {
-		drawtype: Drawtype.mesh,
-		tiles: ["steam_sight_glass.png", "steam_sight_glass_water_texture.png"],
-		mesh: "steam_sight_glass.gltf",
+		drawtype: Drawtype.airlike,
 		sounds: crafter.stoneSound(),
 		groups: { stone: 2 },
 		paramtype: ParamType1.light,
 		paramtype2: ParamType2["4dir"],
 		sunlight_propagates: true,
+		on_timer(position, elapsed) {
+			manipulateSightGlassEntity(position, getOrCreateEntity(position));
+			timerStart(position);
+		},
+		on_construct(position) {
+			getOrCreateEntity(position);
+			timerStart(position);
+		},
+		on_destruct(position) {
+			const hash = core.hash_node_position(position);
+			const ent = sightGlassEntities.get(hash);
+
+			if (ent == null) {
+				return;
+			}
+
+			ent.remove();
+			sightGlassEntities.delete(hash);
+		},
 	});
 }
