8b014f66a1b5b6210953ac59614ef433c1d52c2e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-07 19:11:12 -0400

    Update boiler.ts

--- source/steam/boiler.ts
+++ source/steam/boiler.ts
@@ -84,7 +84,7 @@ namespace steam {
 		mesh: "steam_boiler.gltf",
 		tiles: ["steam_boiler.png"],
 		paramtype2: ParamType2["4dir"],
-		groups: { stone: 1, pathable: 1, steam: 1 },
+		groups: { stone: 1, pathable: 1, steam: 1, water_vessel: 1 },
 		sounds: crafter.stoneSound(),
 
 		on_rightclick(position, node, clicker, itemStack, pointedThing) {
