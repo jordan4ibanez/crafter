c8d9b125cd760c4dbc3095f38de87f2d5295c9d9
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-08 04:26:53 -0400

    Update ash_pan.ts

--- source/steam/ash_pan.ts
+++ source/steam/ash_pan.ts
@@ -1,3 +1,53 @@
 namespace steam {
-	
+	const states = ["open", "closed"];
+	for (const index of $range(0, 1)) {
+		const currentState = states[index];
+		core.register_node("crafter_steam:ash_pan_" + currentState, {
+			drawtype: Drawtype.mesh,
+			use_texture_alpha: TextureAlpha.clip,
+			mesh: `steam_ash_pan_${currentState}.gltf`,
+			tiles: [
+				"steam_ash_pan_body.png",
+				"steam_ash_pan_doors.png",
+				"steam_ash_pan_lever.png",
+				"stone.png",
+				"dirt.png",
+				
+			],
+			paramtype2: ParamType2["4dir"],
+			groups: { stone: 2, firebox: 1, heat_vessel: 1 },
+			sounds: crafter.stoneSound(),
+
+			on_timer(position, elapsed) {
+				// burnFuelAndDoSideEffects(position, index == 0);
+				// manipulateFireEntity(
+				// 	position,
+				// 	fireboxEntities.getOrCreate(position)
+				// );
+				// timerStart(position);
+			},
+
+			on_construct(position) {
+				// fireboxEntities.getOrCreate(position);
+				// timerStart(position);
+			},
+
+			on_rightclick(position, node, clicker, itemStack, pointedThing) {
+				// const fireboxData = utility.getMeta(position, FireboxMeta);
+				// const itemStackName = itemStack.get_name();
+
+				//? Open/close doors.
+				const newIndex = (index + 1) % 2;
+				const newState = states[newIndex];
+				core.swap_node(position, {
+					name: "crafter_steam:ash_pan_" + newState,
+					param2: node.param2,
+				});
+				core.sound_play("steam_boiler_door", {
+					pos: pointedThing.under!,
+					pitch: (math.random(80, 99) + math.random()) / 100,
+				});
+			},
+		});
+	}
 }
