b5a4f9eec0d4eef71b8473da9b9b467a97787e08
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-07 22:55:58 -0400

    Explain what this piece of shit does

--- source/utility/node_entity_container.ts
+++ source/utility/node_entity_container.ts
@@ -1,3 +1,25 @@
 namespace utility {
-	
+	/**
+	 * This class does 2 things:
+	 * 
+	 * 1.) It is compensating for nodes not having any animation properties.
+	 * Yes, you can have a node with a mesh. But this is static. I do understand that
+	 * this is a reasonably difficult thing to implement. So I am just rolling with it.
+	 * 
+	 * 2.) It is compensating for entities not having UUIDs. So basically, the entity has to
+	 * have ``static_save`` set to false. Unless you like having a very laggy game of course.
+	 * People have been trying to stop this from being implemented into the game.
+	 * So if you think this is a shitty implementation, go argue with them in the IRC or Github issue 
+	 * tracker. Thank you for understanding.
+	 */
+	export class NodeEntityContainer<T extends types.Entity> {
+
+
+
+		constructor(clazz: new () => T) {
+
+			const test = new clazz()
+			print(test.name);
+		}
+	}
 }
