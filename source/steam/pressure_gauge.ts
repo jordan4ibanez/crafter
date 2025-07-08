ffd0e39d76ab6725a5b2bb3816ab7dfced025d30
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2025-07-08 00:44:01 -0400

    That can connect to pipes

--- source/steam/pressure_gauge.ts
+++ source/steam/pressure_gauge.ts
@@ -74,7 +74,7 @@ namespace steam {
 	core.register_node("crafter_steam:pressure_gauge", {
 		drawtype: Drawtype.airlike,
 		sounds: crafter.stoneSound(),
-		groups: { stone: 2 },
+		groups: { stone: 2, steam: 1 },
 		paramtype: ParamType1.light,
 		paramtype2: ParamType2["4dir"],
 		sunlight_propagates: true,
